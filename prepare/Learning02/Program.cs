e8a7c01a476583c5091d7dbc9085540704a6ba77
Alromave <122586145+Alromave@users.noreply.github.com>
2023-05-03 21:35:07 -0600

    class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Operation Manager";
+        job2._company = "Cobrissa";
+        job2._startYear = 2015;
+        job2._endYear = 2023;
+        
+
+        Resume myResume = new Resume();
+        myResume._name = "Alvaro Maldonado";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+    
+    
+
+
+      
     }
 }
